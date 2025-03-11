29111b4b958d53f6f6502ffff60c1c22537a63e9
xp65241 <xp65241@naver.com>
2025-03-11 21:16:24 +0900

    project create

---
+++ test/test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 김성민 on 3/11/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
